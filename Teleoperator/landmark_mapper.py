e9578d8c6770498876499f878fe8805d742a6084
Orion Martin <orionmartin049@gmail.com>
2026-09-20 18:30:22 -0400

    removed a bunch of fluff in wrist_map_to_robot. Now it basically just flips some axes and that's it.

--- Teleoperator/landmark_mapper.py
+++ Teleoperator/landmark_mapper.py
@@ -4,21 +4,8 @@ FIXED_ORIENT = [0.0, 3.14159, 0.0]  # tool pointing down (axis-angle, rad) — v
 
 # this comes in after filter
 def wrist_map_to_robot(d):
-    rx = d[0]*1.2
-    ry = d[1]*1.2
-    rz = d[2]*1.2
-
-    min_dist = 0.1
-    max_dist = 0.8
-
-    dist_from_base = math.sqrt((rx*rx)+(ry*ry)+(rz*rz))
-    if (dist_from_base >max_dist):
-        rx = (rx / dist_from_base) * max_dist
-        ry = (ry / dist_from_base) * max_dist
-        rz = (rz / dist_from_base) * max_dist
-    elif (dist_from_base < min_dist):
-        rx = (rx / dist_from_base) * min_dist
-        ry = (ry / dist_from_base) * min_dist
-        rz = (rz / dist_from_base) * min_dist
+    rx = d[0]
+    ry = d[1]
+    rz = d[2]
 
     return [-rz, rx, -ry, *FIXED_ORIENT]
